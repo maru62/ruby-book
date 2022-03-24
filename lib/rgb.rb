509de567cd75c2235d49d407336d10154e498e1b
Yuuki Ohyama <yohyama@jcom.zaq.ne.jp>
2022-03-24 17:56:43 +0900

    unko

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -1,13 +1,10 @@
+# coding: utf-8
 def to_hex(r, g, b)
     [r, g, b].inject('#') do |hex, n|
         hex + n.to_s(16).rjust(2, '0') # そのままブロック単位でreturnされる
     end
 end
 
-def to_ints(hex)
-    r = hex[1..2]
-    g = hex[3..4]
-    b = hex[5..6]
-    ints = []
-    [0, 0, 0]
+def to_ints(rgb)
+  rgb.scan(/\w\w/).map(&:hex)
 end
