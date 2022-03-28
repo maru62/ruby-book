fc403a8a13c0aba3e7b9153dc301b86081afe142
Yuuki Ohyama <yohyama@jcom.zaq.ne.jp>
2022-03-28 22:04:15 +0900

    誘導つきの課題ってやりづらいけど自分で全部書けるわけでもないんだよな

--- lib/convert_length.rb
+++ lib/convert_length.rb
@@ -1,4 +1,7 @@
+# coding: utf-8
 def convert_length(length, unit_from, unit_to)
-    # まず変換したい諸単位を定義する、んじゃなかった
+  # まず変換したい諸単位を定義する、んじゃなかった
+  # m / m, m / ft, m / in
     units = { 'm' => 1.0, 'ft' => 3.28, 'in' => 39.37 }
+    (length / units[unit_from] * units[unit_to]).round(2)
 end
