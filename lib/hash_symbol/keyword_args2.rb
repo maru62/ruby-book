178a892434f9458183451e20597206bc7c68d024
Yuuki Ohyama <yohyama@jcom.zaq.ne.jp>
2022-03-30 21:44:45 +0900

    ギフハブのパスワードめんどくさい

--- lib/hash_symbol/keyword_args2.rb
+++ lib/hash_symbol/keyword_args2.rb
@@ -2,3 +2,17 @@
 # 疑似コードなので実行できない
 =begin
 
+# 引数のハッシュにダラダラいろいろついてきたときに必要なもの以外を捨てる
+def buy_burger(menu, drink: true, potato: true, **others)
+    ......
+end
+
+buy_burger('fish', drink: true, potato: false, salad:true, chicken: false)
+# salad以降が無視される
+
+=end
+
+
+# キーワード引数部をハッシュで受け付けてみる
+def buy_burger(menu, options = {})
+  puts 
