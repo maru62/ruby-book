d1a92fb9802f45f1366b115f9c5c33eb4533eecc
Yuuki Ohyama <yohyama@jcom.zaq.ne.jp>
2022-04-01 21:36:28 +0900

    明日はもうすこし頑張りましょう

--- lib/hash_symbol/init_values.rb
+++ lib/hash_symbol/init_values.rb
@@ -9,11 +9,23 @@ hash2 = Hash.new('todesking')
 p hash2[:foo]
 # キーは指定しなくていいの？難しい
 
-a = hash2[:unko]
-b = hash2[:death]
+value1 = hash2[:unko]
+value2 = hash2[:death]
 
-p a
-p b
+p value1
+p value2
 
-# aとbは同一オブジェクト
-p a.equal?(b)
+# aとbは同一オブジェクトの異なる参照にすぎない
+p value1.equal?(value2)
+
+# なので片方に破壊的変更をするともう片方も変わる
+value1.upcase!
+# 書いてから思ったけどここから下だけ変わるんだ
+p value1
+p value2 
+
+# ハッシュ自体は空、！！？！？
+p hash2
+
+# ブロックを噛ませたほうがよい
+hash3 
