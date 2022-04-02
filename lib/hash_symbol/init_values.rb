cc852441f29975d99dd68271a6edc2dbf78eb841
Yuuki Ohyama <yohyama@jcom.zaq.ne.jp>
2022-04-02 22:42:38 +0900

    休みだとあんまり何もやれてないので悲しい

--- lib/hash_symbol/init_values.rb
+++ lib/hash_symbol/init_values.rb
@@ -4,6 +4,9 @@ hash1 = {}
 p hash1[:fuba]
 
 # Hash.newメソッド
+
+### そもそもキーのないハッシュという意味不明な物体をどう扱うのか！！？？！
+
 ## キーがない場合はtodeskingを返す
 hash2 = Hash.new('todesking')
 p hash2[:foo]
@@ -28,4 +31,27 @@ p value2
 p hash2
 
 # ブロックを噛ませたほうがよい
-hash3 
+hash3 = Hash.new { 'yawamuge' }
+value3 = hash3[:unko]
+value4 = hash3[:death]
+p value3
+p value4
+
+# これはこんどは違うオブジェクトになる、ブロック実行が挟まれるため
+p value3.equal?(value4)
+
+# なので片方を破壊的変更してももう片方は不変
+p value3.upcase!
+p value4
+
+# ハッシュは空のまま
+p hash3
+
+## キーと初期値を同時に設定する（普通はそうしてくれ）
+# Hash.newメソッドにブロックを与える
+hash4 = Hash.new { |hash, key| hash[key] = 'der todesking' }
+p hash4[:gyoe]
+p hash4[:abo]
+
+# ハッシュは空ではない
+p hash4
